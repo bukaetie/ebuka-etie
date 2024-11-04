590d63d660d4e3a3eae326fdc54e83aef043e021
bukaetie <etieebuka@gmail.com>
2024-11-04 21:11:20 +0100

    change write up in About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <Details 
     image={'/pic.jpg'}
     title={'About Me'}
-    about={'Welcome! I’m Ebuka Etie, a Software Developer with a strong foundation in IT and a passion for innovative technology. Currently advancing my expertise as an IT specialist in software development, I bring analytical thinking, adaptability, and a commitment to lifelong learning. Driven by curiosity and the pursuit of impactful solutions, I’m dedicated to mastering modern development practices and contributing to transformative projects in tech.'}/>
+    about={'I’m Ebuka Etie, a Software Developer with a strong foundation in IT and a passion for innovative technology. Currently advancing my expertise as an IT specialist in software development, I bring analytical thinking, adaptability, and a commitment to lifelong learning. Driven by curiosity and the pursuit of impactful solutions, I’m dedicated to mastering modern development practices and contributing to transformative projects in tech.'}/>
   )
 }
 
